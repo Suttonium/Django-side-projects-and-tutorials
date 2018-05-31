8a4c11a480340168a908103dd577d77baeedcc90
Raymond Sutton <raymond.sutton.15@cnu.edu>
2018-05-31 01:10:44 -0400

    Delete urls.py

--- DJANGO_LEVEL_FIVE/user_authentication/authentication_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from authentication_app import views
-
-app_name = 'authentication_app'
-
-urlpatterns = [
-    url(r'^register/$', views.register, name='register'),
-    url(r'^user_login/$', views.user_login, name='user_login')
-]
